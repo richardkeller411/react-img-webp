7f9f4adb69190890adeef3cf239a18130b245466
richardkeller411 <richard@kellersystems.com>
2019-12-03 10:37:11 -0800

    Test Lazy Sizes on SSR Context...

--- src/Context/Image.tsx
+++ src/Context/Image.tsx
@@ -2,16 +2,35 @@ import * as React from "react";
 import { WebP, WebPValue } from "./WebP";
 
 export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-    webP?: string;
+  webP?: string;
 }
 
-export const Image: React.FunctionComponent<ImageProps> = React.memo((props) => {
-    const childProps = { ...props };
-    delete childProps.webP;
+export const Image: React.FunctionComponent<ImageProps> = React.memo(props => {
+  const childProps = { ...props };
+  delete childProps.webP;
 
+  if (props.useLazySizes) {
     return (
-        <WebP.Consumer>
-            {(value: WebPValue) => <img {...childProps} src={value.supportWebP && props.webP || props.src} />}
-        </WebP.Consumer>
+      <WebP.Consumer>
+        {(value: WebPValue) => (
+          <img
+            {...childProps}
+            data-src={(props.useWebP && props.webP) || props.src}
+            className="lazyload"
+          />
+        )}
+      </WebP.Consumer>
     );
+  }
+
+  return (
+    <WebP.Consumer>
+      {(value: WebPValue) => (
+        <img
+          {...childProps}
+          src={(value.supportWebP && props.webP) || props.src}
+        />
+      )}
+    </WebP.Consumer>
+  );
 });
